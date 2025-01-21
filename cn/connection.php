6e5fe817248f1c79528943c6fdd23779a56ddbcf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2025-01-21 17:17:57 -0500

    ADD config and connection files

---
+++ cn/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'nameofthehost';
+$dbname = 'nameofDB';
+$username = 'USER';
+$password = 'PASS';
+?>
---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+// set variables
+// Will get form DB
+$candidato = 'Rodrigo Zarate';
+$numero = '03';
+$partido = 'Dignidad y Compromiso';
+$logo = 'img/logo.png';
+?>
