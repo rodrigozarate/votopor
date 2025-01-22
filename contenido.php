37907035ac008605a45e3abba52f11e29885e6fc
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2025-01-21 20:58:05 -0500

    ADD style files

--- contenido.php
+++ contenido.php
@@ -1,29 +1,11 @@
-<body>
-<header class="p-3 bg-dark text-white">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Apoyar</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Propuestas</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Partido</a></li>
-        </ul>
-      </div>
-    </div>
-  </header>
 <div class="container">
 	<div class="col d-flex justify-content-center">
-		<h1>Voto por <?php echo $candidato; ?></h1>
-		<main class="px-3">
+		<main class="px-3 my-4">
 			<div class="card" style="width: 18rem;">
 				<img class="card-img-top" src="img/candidato.png" alt="<?php echo $candidato; ?>" />
-				<img class="card-img-number" src="img/number.png" alt="<?php echo $numero; ?>" />
+				<img class="card-img-number" src="img/numero.png" alt="<?php echo $numero; ?>" />
 				<div class="card-body">
-					<h5 class="card-title"><?php echo $candidato; ?></h5>
-					<h6>Registro de Usuario</h6>
+					<h5 class="card-title">Apoyar</h5>
 					<p class="card-text"><?php echo $slogan; ?></p>
 					<form action="registro.php" method="POST">
 						<label for="nombre">Nombre:</label><br />
--- encabezado.php
+++ encabezado.php
@@ -9,4 +9,23 @@ include_once('config/config.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $titulo; ?></title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="css/style.css" rel="stylesheet">
 </head>
+<body>
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" fill="#442f7f" width="40" height="32" role="img" aria-label="Home"><use xlink:href="#home"/></svg>
+        </a>
+
+		<h1 class="col-12 col-md-6 mb-2 justify-content-center mb-md-0"><?php echo $candidato; ?></h1>
+
+        <ul class="nav col-md-3 text-end">
+          <li><a href="#apoyar" class="nav-link px-2 text-secondary">Apoyar</a></li>
+          <li><a href="propuestas" class="nav-link px-2 text-white">Propuestas</a></li>
+          <li><a href="https://dignidadycompromiso.co/" class="nav-link px-2 text-white">Partido</a></li>
+        </ul>
+      </div>
+    </div>
+  </header>
